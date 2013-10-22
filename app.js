243867ff66209705b11f2f434fecd84cb91c57c5
OpenShift guest <wuleicanada@msn.com>
2013-10-22 09:06:27 -0400

    added Google site verification to app.js

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/googleefc2e439fd107693.html', function(req, res){
+  res.send('google-site-verification: googleefc2e439fd107693.html');
+});
+
 app.listen(process.env.PORT || 3000);
