a5edf53098531e55523ed03e80de9e224de52647
OpenShift guest <wuleicanada@msn.com>
2013-10-23 11:03:30 -0400

    Baidu webmaster site verification

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/googleefc2e439fd107693.html', function(req, res){
   res.send('google-site-verification: googleefc2e439fd107693.html');
 });
 
+app.get('/baidu_verify_dX89zb2pJU.html', function(req, res){
+  res.send('dX89zb2pJU');
+});
+
 app.listen(process.env.PORT || 3000);
